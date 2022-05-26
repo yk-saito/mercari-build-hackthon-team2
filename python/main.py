34f03a2e56d46e81981cc7bddf9ef4135b72d915
nsmryk <yuki.nishimura369@gmail.com>
2022-05-26 18:34:23 +0900

    Create a database using items.db

--- python/main.py
+++ python/main.py
@@ -2,11 +2,15 @@ import os
 import logging
 import pathlib
 import sqlite3
-from fastapi import FastAPI, Form, HTTPException
+import hashlib
+import shutil
+import sys
+from fastapi import FastAPI, Form, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+ 
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "image"
@@ -19,25 +23,63 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class DBConnection:
+    def __init__(self, dbpath="../db/mercari.sqlite3"):
+        try:
+            self.connection = sqlite3.connect(dbpath)
+            self.cursor = self.connection.cursor()
+            with open('../db/items.db') as f:
+                self.cursor.executescript(f.read())
+            self.connection.commit()
+        except Exception as e:
+            sys.exit(e)
+
+    def execute_insert(self, sql, parameter=[]):
+        try:
+            self.cursor.execute(sql, parameter)
+            self.connection.commit()
+        except Exception as e:
+            sys.exit(e)
+
+    def execute_select(self, sql, parameter=[]):
+        try:
+            sql_result = self.cursor.execute(sql, parameter).fetchall()
+            self.cursor.commit()
+        except Exception as e:
+            sys.exit(e)
+
+        return sql_result
+
+    def __del__(self):
+        try:
+            self.connection.close()
+        except Exception as e:
+            sys.exit(e)
+
+db = DBConnection()
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
 @app.get("/items")
 def get_item():
-    with sqlite3.connect("../db/mercari.sqlite3") as conn:
-        #カーソル
-        cur = conn.cursor()
-        #データの取得
-        cur.execute("SELECT * FROM items")
-        item_obj = cur.fetchall()
-        #変更の反映
-        conn.commit()
-    
+    sql = "SELECT * FROM items"
+    item_obj = db.execute_select(sql)
     return item_obj
 
 @app.post("/items")
-def add_item(name: str = Form(...), category: str = Form(...)):
+def add_item(name: str = Form(...), category_id: str = Form(...), image: UploadFile = File(...)):
+    image_title = pathlib.Path(image.filename).stem
+    image_suffix = pathlib.Path(image.filename).suffix
+    image_filename = hashlib.sha256(image_title.encode()).hexdigest() + image_suffix
+    
+    #画像を保存
+    filepath = f"../images/{image_filename}"
+    with open(filepath, "w+b") as f:
+        shutil.copyfileobj(image.file, f)
+
+    #DB
     with sqlite3.connect("../db/mercari.sqlite3") as conn:
         #カーソル
         cur = conn.cursor()
@@ -45,12 +87,13 @@ def add_item(name: str = Form(...), category: str = Form(...)):
         cur.execute("CREATE TABLE IF NOT EXISTS items (\
             id INTEGER AUTO_INCREMENT, \
             name TEXT NOT NULL,\
-            category TEXT NOT NULL\
+            category_id INTEGER\
+            image_filename TEXT NOT NULL\
             )")
 
         #受け取ったデータを追加
-        #(?, ?)はプレースホルダ
-        cur.execute("INSERT INTO items (name, category) VALUES (?, ?)", (name, category))
+        #(?, ?, ?)はプレースホルダ
+        cur.execute("INSERT INTO items (name, category_id, image_filename) VALUES (?, ?, ?)", (name, category_id, image_filename))
         #変更の反映
         conn.commit()
 
@@ -67,7 +110,7 @@ def search_item(keyword: str):
         item = cur.fetchall()
         item_dict = {"items" : []}
         for i in range(len(item)):
-                item_dict["items"].append({"name": item[i][0], "category": item[i][1]})
+                item_dict["items"].append({"name": item[i][0], "category": item[i][1], "image_filename": item[i][2]})
         
     return item_dict
     
